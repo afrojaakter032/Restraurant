10a653920e867b516abbdacb15ca8f9f67a17a30
afrojaakter032 <afroja.cu.cse@gmail.com>
2023-08-03 00:44:43 +0600

    js

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,14 +8,37 @@ window.addEventListener("load", function () {
 // menubar hide for mobaile device
 
 document.querySelector('.menubar-icon').addEventListener('click', function(){
-    document.querySelector('.navbar').style.left = '0px';
+    document.querySelector('.navbar').classList.add('open');
     
 
 });
 
 document.querySelector('.menu-close-btn').addEventListener('click', function(){
-    document.querySelector('.navbar').style.left = '-400px';
+    
+    document.querySelector('.navbar').classList.remove('open');
     
 
 });
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
       
